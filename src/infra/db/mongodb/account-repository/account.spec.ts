77a1739494ed013c7ffb77552e9a4c6d0eb547f3
Vagner Wentz <wentz.vagner@gmail.com>
2020-10-29 14:03:03 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,28 @@
+import { MongoHelper } from '../helpers/mongo-helper';
+
+import { AccountMongoRepository } from './account';
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  it('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository();
+    const account = await sut.add({
+      name: 'valid-name',
+      email: 'valid-email@mail.com',
+      password: 'valid-password',
+    });
+
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe('valid-name');
+    expect(account.email).toBe('valid-email@mail.com');
+    expect(account.password).toBe('valid-password');
+  });
+});
