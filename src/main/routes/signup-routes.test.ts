9082895ec32a4437c56c51084008ff7905726394
Vagner Wentz <wentz.vagner@gmail.com>
2020-10-31 13:22:40 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,13 +1,28 @@
 import request from 'supertest';
 import app from '../config/app';
 
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
+
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  });
+
   it('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
       .send({
-        name: 'Vagner',
-        email: 'vagner@gmail.com',
+        name: 'John Doe',
+        email: 'john@mail.com',
         password: '123',
         passwordConfirmation: '123',
       })
